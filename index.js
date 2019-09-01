ed0f9ac9d4fbca9ed7a3965c3e3496a0a5dd8519
Tony <kopanewanton@gmail.com>
2019-09-01 13:28:27 +0300

    add func on calc

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ class Calculator {
   constructor(where){
     const div = document.createElement('div');
     const input = document.createElement('input');
+    let sign = null;
+    input.value = 0;
       
     const chars = [
         '7', '8', '9', '/',
@@ -18,24 +20,25 @@ class Calculator {
         if(char === '+' || char === '-' || char === '/' || char === '*')
           button.className = 'sign';
 
-        button.onclick = function() {
-          //if(isFinite(char))
-
-         console.log('[char]', char);
-
-         switch(char) {
-           case '+':
-           case '-':
-           case '*':
-           case '/':
-         }
-
-          if(input.value.includes('.') && char === '.') return;
-          if(char === 'c') {
-            input.value = '';
-            return;
+        button.onclick = () => {
+          const isDouble = !!(this.doubleSign(input.value) && this.doubleSign(input.value) !== char);
+          if(this.doubleSign(input.value) === true) input.value = ''; 
+          
+          switch(char) {
+            case '+':
+            case '-':
+            case '*':
+            case '/': if(isDouble) return; sign = char; break;
+            case '=': return input.value = this.calculate(input.value, sign);
           }
 
+          if(input.value.includes('.') && char === '.')
+            if(!this.doubleDotted(input.value, sign)) return;
+          if(char === 'c') {
+            input.value = 0;
+            return;
+          }
+          if(input.value === '0' && isFinite(char)) input.value = '';
           input.value += char;
         };
 
@@ -48,6 +51,43 @@ class Calculator {
       for(const btn of buttons) div.append(btn);
 
       document.getElementById(where).append(div);
+  }
+
+  calculate(str, sign) {
+    const [ firstOperand, secondOperand ] = str.split(sign);
+    if(!isFinite(firstOperand) || !isFinite(secondOperand)) return 0;
+         switch(sign) {
+           case '+': return +firstOperand + +secondOperand;
+           case '-': return +firstOperand - +secondOperand;
+           case '*': return +firstOperand * +secondOperand;
+           case '/': return +firstOperand / +secondOperand;
+         }
+    return 0;
+  };
+
+  doubleSign(str) {
+    if(!isFinite(str)){
+      if(str.includes('+')) return '+';
+      if(str.includes('-')) return '-';
+      if(str.includes('*')) return '*';
+      if(str.includes('/')) return '/';
+      return true;
+    }
+    return false;
+  };
+
+  doubleDotted(str, sign) {
+    console.log('[isNotSign]', !!(!sign));
+    console.log('[isFiniteStr]', isFinite(str));
+    //console.log('[isFiniteStrLastChar]', !isFinite(str.slice(str.length-1)));
+    if(!sign) return false;
+    if(isFinite(str)) return false;
+    //if(!isFinite(str.slice(str.length-1))) return false;
+
+    const lastOperand = str.split(sign).pop();
+    console.log('[lastOperand]', lastOperand);
+    if(lastOperand.includes('.')) return false;
+    return true;
   }
 };
 
